5cc1a40b16df2470779321b4fd27feafe10d225c
RogerPina2 <roger.pina12@hotmail.com>
2021-04-08 18:52:19 -0300

    Movimentação do player

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterController2D : MonoBehaviour
+{
+    [Header("Events")][Space] public UnityEvent OnLandEvent;
+
+    public Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    public bool isGrounded;
+    [SerializeField] private float jumpForce = 250f;
+    [SerializeField] private float rollForce = 1.2f;
+    [SerializeField] private Collider2D rollDisableCollider;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
+    }
+
+    public void Move(float move, bool jump, bool roll)
+    {
+        transform.Translate(move * 5.0f * Time.deltaTime, 0f, 0f);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+
+        if (isGrounded) 
+        {
+            if (jump) 
+            {
+                rb.AddForce(new Vector2(0f, jumpForce));
+                isGrounded = false;
+            }
+
+            if (roll) 
+            {
+                if (facingRight)
+                    rb.velocity = new Vector2(rollForce, rb.velocity.y);
+                else  
+                    rb.velocity = new Vector2(-1 * rollForce, rb.velocity.y);
+
+                if (rollDisableCollider != null)
+                    StartCoroutine(WaitToEnable());
+            }
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            OnLandEvent.Invoke();
+        }
+    }
+
+    private IEnumerator WaitToEnable()
+    {
+        rollDisableCollider.enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        rollDisableCollider.enabled = true;
+    }
+
+
+    public void Fight(bool attack, bool block)
+    {
+        if (attack) {
+            //
+        }
+
+        if (block) {
+            //
+        }
+    }
+}
---
+++ Assets/Scripts/HeroKnightMoviment.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroKnightMoviment : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;   
+    float horizontalMove = 0f;
+    bool jump;
+    bool roll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Hero Knight Run
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        animator.SetBool("Grounded", controller.isGrounded);
+
+        // Hero Knight Jump
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+            animator.SetBool("Jump", true);
+        }
+        animator.SetFloat("AirSpeedY", controller.rb.velocity.y);
+
+        // Roll
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !roll) {
+            roll = true;
+            animator.SetBool("Roll", true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump, roll);
+        jump = false;
+        roll = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("Jump", false);
+    }
+}
