88dac793f32b72a62199544e34a0be849fcdf7b2
Bruno Domingues <bruno_signorelli@outlook.com>
2021-04-25 19:21:52 -0300

    Added main menu and tutorial scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,29 +14,30 @@ public class GameManager
         return _instance;
     }
 
-    public delegate void ChangeStateDelegate();
-    public static ChangeStateDelegate changeStateDelegate;
+    //public delegate void ChangeStateDelegate();
+    //public static ChangeStateDelegate changeStateDelegate;
 
-    public enum GameState {MENU, GAME, PAUSE, ENDGAME};
+    //public enum GameState {MENU, GAME, PAUSE, ENDGAME};
 
-    public GameState gameState {get; private set;}
+    //public GameState gameState {get; private set;}
     public int lifes;
     public int points;
+    public bool isPaused = false;
 
     private GameManager()
     {
         lifes = 3;
         points = 0;
-        gameState = GameState.MENU;
+        //gameState = GameState.MENU;
     }
 
-    public void ChangeState(GameState nextState)
-    {
-        if (nextState == GameState.GAME) Reset();
+    //public void ChangeState(GameState nextState)
+    //{
+    //    if (nextState == GameState.GAME) Reset();
 
-        gameState = nextState;
-        changeStateDelegate();
-    }
+    //    gameState = nextState;
+    //    changeStateDelegate();
+    //}
 
     private void Reset()
     {
