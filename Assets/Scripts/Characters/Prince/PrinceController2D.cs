30c4988b1f15d64f8950178a76875d0979fda253
RogerPina2 <roger.pina12@hotmail.com>
2021-04-25 16:38:16 -0300

    Refactoring Prince Codes

---
+++ Assets/Scripts/Characters/Prince/PrinceController2D.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PrinceController2D : MonoBehaviour
+{
+    GameManager gm;
+    public  Rigidbody2D  rb;
+    public  Animator     animator;
+    [Header("Events")] [Space] public UnityEvent OnLandEvent;
+
+    // Game Managing
+    public bool inGame;
+
+    // Movimentation
+    private bool    facingRight = true;
+    public  bool    isGrounded;
+
+    [SerializeField] public  float   jumpForce = 300f;
+
+    [SerializeField] private Collider2D rollDisableCollider;
+    [SerializeField] private float rollForce = 1.2f;
+
+    void Start()
+    {
+        gm = GameManager.GetInstance();
+    }
+
+    void Update()
+    {
+        // Game Managing
+        if (gm.gameState != GameManager.GameState.GAME)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Escape) && gm.gameState == GameManager.GameState.GAME)
+        {
+            gm.ChangeState(GameManager.GameState.PAUSE);
+        }
+
+        // Morte por queda
+        if (transform.position.y < -8)
+        {
+            Reset();
+        }
+
+    }
+
+    private void Awake()
+    {
+        rb          = GetComponent<Rigidbody2D>();
+        animator    = GetComponent<Animator>();
+
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
+    } 
+
+    public void Move(bool run, float move, bool jump, bool roll)
+    {
+        // Run
+        transform.Translate(move * 4.5f * Time.deltaTime, 0f, 0f);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+
+        // Jump
+        if (jump && isGrounded) 
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isGrounded = false;
+        }
+        
+        // Roll
+        if (roll && isGrounded)
+        {
+            if (facingRight)
+                rb.velocity = new Vector2(rollForce, rb.velocity.y);
+            else
+                rb.velocity = new Vector2(-1 * rollForce, rb.velocity.y);
+
+            if (rollDisableCollider != null)
+                StartCoroutine(WaitToEnable());
+        }
+    }
+
+    // private void Combat()
+    // {
+    //     // Attack
+    //     if (attack) {
+    //         animator.SetTrigger("Attack" + currentAttack);
+    //         attack = false;
+    //     }
+    // }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            OnLandEvent.Invoke();
+        }
+    }
+
+    private IEnumerator WaitToEnable()
+    {
+        rollDisableCollider.enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        rollDisableCollider.enabled = true;
+    }
+
+    private void Reset()
+    {
+        if (gm.lifes <= 0 && gm.gameState == GameManager.GameState.GAME)
+            gm.ChangeState(GameManager.GameState.ENDGAME);
+
+        Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        transform.position = new Vector3(-6, 0, 0);
+    
+        gm.lifes--;
+    }
+}
---
+++ Assets/Scripts/Characters/Prince/PrinceMoviment.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrinceMoviment : MonoBehaviour
+{
+    GameManager gm;
+    public  Animator     animator;
+    public  PrinceController2D controller;
+
+    // Run
+    [SerializeField] 
+    public  float   runSpeed = 40f;
+    private float   horizontalMove = 0f;
+    private bool    run = true;
+
+    // Jump
+    private bool    jump;
+ 
+    // Roll
+    private bool    roll;
+
+    // Attack
+    private float   timeSinceAttack = 0f;
+    private int     currentAttack = 0;
+    private bool    attack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        gm = GameManager.GetInstance();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gm.gameState != GameManager.GameState.GAME)
+        {
+            if (controller.rb.velocity.y != 0) 
+                controller.rb.velocity = new Vector2(0f, 0f);
+            controller.rb.gravityScale = 0;
+            animator.SetFloat("Speed", 0);
+            return;
+        }
+        controller.rb.gravityScale = 1;
+
+        // === Prince Movimentation ===
+        animator.SetBool("Grounded", controller.isGrounded);
+        animator.SetFloat("AirSpeedY", controller.rb.velocity.y);
+        timeSinceAttack += Time.deltaTime;
+
+        // Run
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && !jump) 
+        {
+            animator.SetBool("Jump", true);
+            jump = true;
+            roll = false;
+        }
+
+        // Roll
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !roll)
+        {
+            animator.SetBool("Roll", true);
+            roll = true;
+        }
+
+        // Attack
+        if (Input.GetMouseButtonDown(0) && timeSinceAttack > 0.25f && !roll)
+        {
+            attack = true;
+
+            currentAttack++;
+
+            if (currentAttack > 3)
+                currentAttack = 1;
+
+            if (timeSinceAttack > 1.0f)
+                currentAttack = 1;
+
+            timeSinceAttack = 0f;
+        }
+    }
+
+    void FixedUpdate() {
+        if (gm.gameState != GameManager.GameState.GAME)
+            return;
+        
+        controller.Move(run, horizontalMove * Time.fixedDeltaTime, jump, roll);
+        jump = false;
+        roll = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("Jump", false);
+    }
+
+    // Animation Events
+    // Called in end of roll animation.
+    void AE_ResetRoll()
+    {
+        roll = false;
+    }
+}
